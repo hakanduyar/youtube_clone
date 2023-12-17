b9c85c3e1fb4c94309b740cf189a7ba70a71f5dd
rikords <iamhakanduyar@gmail.com>
2023-12-17 12:42:04 +0300

    Update components to use new `VideoCard` and `ChannelCard` components.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,10 +5,13 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { SideBar, Videos } from "./";
 
 const Feed = () => {
-  const [selectedCategory, setselectedCategory] = useState("New");
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
   }, [selectedCategory]);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -21,7 +24,7 @@ const Feed = () => {
       >
         <SideBar
           selectedCategory={selectedCategory}
-          setselectedCategory={setselectedCategory}
+          setSelectedCategory={setSelectedCategory}
         />
         <Typography
           className="copyright"
@@ -39,10 +42,10 @@ const Feed = () => {
           mb={2}
           sx={{ color: "white" }}
         >
-          New <span style={{ color: "#F31503" }}>videos</span>
+          {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
         </Typography>
 
-        <Videos videos={[]} />
+        <Videos videos={[videos]} />
       </Box>
     </Stack>
   );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@ import { Stack } from "@mui/material";
 
 import { categories } from "../utils/constants";
 
-const SideBar = ({ selectedCategory, selectedCategory }) => (
+const SideBar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -14,6 +14,7 @@ const SideBar = ({ selectedCategory, selectedCategory }) => (
     {categories.map((category) => (
       <button
         className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
         style={{
           background: category.name === selectedCategory && "#FC1503",
           color: "white",
