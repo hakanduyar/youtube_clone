7c0c6d29ab6f2641401e9468fdb89ef7cb87ab4d
rikords <iamhakanduyar@gmail.com>
2023-12-18 23:20:12 +0300

    fix bug

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -45,7 +45,7 @@ const Feed = () => {
           {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
         </Typography>
 
-        <Videos videos={[videos]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
