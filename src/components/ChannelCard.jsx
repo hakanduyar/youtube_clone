0aa8f9759a44d23ac65e26331161f957d1d825f7
rikords <iamhakanduyar@gmail.com>
2023-12-22 23:27:08 +0300

    Update the UI components for videos and channels

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
+import { demoProfilePicture } from "../utils/constants";
+
+const ChannelCard = ({ channelDetail }) => {
+  return;
+  <Box sx={{ boxShadow: "none", borderRadius: "20px" }}>
+    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          color: "#fff",
+        }}
+      ></CardContent>
+    </Link>
+  </Box>;
 };
 
 export default ChannelCard;
