b40f0f9d19cda82167bfa11a39d58894017110b5
rikords <iamhakanduyar@gmail.com>
2024-01-12 20:58:05 +0300

    Update ChannelDetails component to fetch and display channel details and videos using the YouTube API.

--- src/components/ChannelDetails.jsx
+++ src/components/ChannelDetails.jsx
@@ -3,9 +3,26 @@ import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
-const ChannelDetails = () => {
-  return <div>ChannelDetails</div>;
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+
+  console.log(channelDetail, videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromAPI(`search?channelId=&${id}part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  return <div>{id}</div>;
 };
 
-export default ChannelDetails;
+export default ChannelDetail;
