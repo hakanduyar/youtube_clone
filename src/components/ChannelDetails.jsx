0a6732ebb95669a92d79c90b3c96ac21d40c807a
rikords <iamhakanduyar@gmail.com>
2024-01-14 23:19:11 +0300

    Update ChannelCard and ChannelDetails components

--- src/components/ChannelDetails.jsx
+++ src/components/ChannelDetails.jsx
@@ -17,12 +17,27 @@ const ChannelDetail = () => {
     fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
       setChannelDetail(data?.items[0])
     );
-    fetchFromAPI(`search?channelId=&${id}part=snippet&order=date`).then(
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
       (data) => setVideos(data?.items)
     );
   }, [id]);
 
-  return <div>{id}</div>;
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(0, 238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0, 212,255,1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box />
+    </Box>
+  );
 };
 
 export default ChannelDetail;
